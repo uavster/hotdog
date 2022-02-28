8c17669009a00de080412e937d24b77158159230
b52 <ignacio.mellado@gmail.com>
2022-02-27 23:12:58 -0800

    Saves trajectory and dumps it when tethered.

--- period_measure.ino
+++ period_measure.ino
@@ -79,8 +79,8 @@ EventRingBuffer event_buffer;
 
 class Point {
   public:
-    double x;
-    double y;
+    float x;
+    float y;
 
     Point(): x(0), y(0) {}
 };
@@ -89,7 +89,7 @@ class RobotState {
   private:
     int left_wheel_ticks_;
     int right_wheel_ticks_;
-    double angle_;
+    float angle_;
     Point center_;
     bool left_wheel_moving_backward_;
     bool right_wheel_moving_backward_;
@@ -106,7 +106,7 @@ class RobotState {
       }
       left_wheel_ticks_ += left_ticks_inc;
       right_wheel_ticks_ += right_ticks_inc;
-      double distance_inc = (kRadiansPerWheelTick * kWheelRadius * (left_ticks_inc + right_ticks_inc)) / 2;
+      float distance_inc = (kRadiansPerWheelTick * kWheelRadius * (left_ticks_inc + right_ticks_inc)) / 2;
       angle_ = ((kRadiansPerWheelTick * kWheelRadius) * (right_wheel_ticks_ - left_wheel_ticks_)) / kRobotDistanceBetweenTireCenters;
       center_.x += distance_inc * cos(angle_);
       center_.y += distance_inc * sin(angle_);
@@ -124,7 +124,7 @@ class RobotState {
       return center_;
     }
 
-    const double Angle() {
+    const float Angle() {
       return angle_;
     }
 };
@@ -188,6 +188,9 @@ void init_motor_control() {
   FTM1_C0SC = FTM_CSC_MSB | FTM_CSC_ELSB;
 
   FTM1_CNTIN = 0;
+
+  set_duty_cycle_left(0);
+  set_duty_cycle_right(0);
 }
 
 void set_duty_cycle_right(float s) {
@@ -241,7 +244,7 @@ void set_duty_cycle_left(float s) {
 void setup() {
   pinMode(ledPin, OUTPUT);
 
-  Serial.begin(38400);
+  Serial.begin(9600);
 
   timer0_num_overflows = 0;
 
@@ -352,14 +355,24 @@ void Uint64ToString(uint64_t number, char *str) {
 
 //char format_buffer[32];
 
-//enum { CIRCLING_LEFT, CIRCLING_RIGHT } state = CIRCLING_LEFT;
+enum { INIT, CIRCLING_LEFT, CIRCLING_RIGHT, WAIT_FOR_INPUT, SEND_TRAJECTORY, DONE } state = INIT;
+int64_t cycles = 0;
 
-//int64_t cycles = 0;
-
-//#define kWaitCycles 5000000L
-//#define kInnerDutyCycle 0.25
+#define kWaitCycles 100
+#define kInnerDutyCycle 0.25
 
 RobotState robot_state;
+
+typedef struct {
+  Point center;
+  float angle;
+} TrajectoryPoint;
+
+#define kMaxTrajectoryPoints  3000
+int cur_trajectory_point = 0;
+TrajectoryPoint trajectory[kMaxTrajectoryPoints];
+int send_index = 0;
+char tmp[128];
 
 void loop() {
 /*  if (event_buffer.NumEvents() > 0) {
@@ -385,31 +398,54 @@ void loop() {
   t += 0.00005f;
 */
   
-/*  switch(state) {
+  switch(state) {
+    case INIT:
+      set_duty_cycle_left(kInnerDutyCycle);
+      set_duty_cycle_right(1.0);
+      cycles = 0;
+      state = CIRCLING_LEFT;
+      break;
     case CIRCLING_LEFT:
       if (cycles > kWaitCycles) { 
-        set_duty_cycle_left(kInnerDutyCycle);
-        set_duty_cycle_right(1.0);
+        set_duty_cycle_left(1.0);
+        set_duty_cycle_right(kInnerDutyCycle);
         cycles = 0;
         state = CIRCLING_RIGHT;
       }
       break;
     case CIRCLING_RIGHT:
       if (cycles > kWaitCycles) { 
-        set_duty_cycle_left(1.0);
-        set_duty_cycle_right(kInnerDutyCycle);
+        set_duty_cycle_left(0);
+        set_duty_cycle_right(0);
         cycles = 0;
-        state = CIRCLING_LEFT;
+        state = WAIT_FOR_INPUT;
       }
       break;
+    case WAIT_FOR_INPUT:
+      if (Serial.available() > 0 && Serial.read() == 'n') {
+        state = SEND_TRAJECTORY;
+        send_index = 0;
+        sprintf(tmp, "Sending trajectory with %d points...", cur_trajectory_point);
+        Serial.println(tmp);
+      }
+      break;
+    case SEND_TRAJECTORY:
+      sprintf(tmp, "%f,%f,%f", trajectory[send_index].center.x, trajectory[send_index].center.y, trajectory[send_index].angle);
+      Serial.println(tmp);
+      ++send_index;
+      if (send_index >= cur_trajectory_point) {
+        state = DONE;
+      }
+      break;
+    case DONE:
+      break;
   }
-  ++cycles;
-*/
 
   if (event_buffer.NumEvents() > 0) {
     int left_ticks = 0;
     int right_ticks = 0;
     while (event_buffer.NumEvents() > 0) {
+      ++cycles;
       const Event &event = event_buffer.Read();
       switch(event.type) {
         case kLeftWheelTick:
@@ -434,11 +470,13 @@ void loop() {
           break;
       }
     }
-    char text_buffer[32];
-    sprintf(text_buffer, "(%.02f, %.02f) %.02f", robot_state.Center().x, robot_state.Center().y, (robot_state.Angle() * 180.0) / M_PI);
-    Serial.println(text_buffer);
+    if ((state == INIT || state == CIRCLING_LEFT || state == CIRCLING_RIGHT) && cur_trajectory_point < kMaxTrajectoryPoints) {
+      trajectory[cur_trajectory_point].center = robot_state.Center();
+      trajectory[cur_trajectory_point].angle = robot_state.Angle();
+      ++cur_trajectory_point;
+    }
   }
 
-  set_duty_cycle_left(1.0f);
-  set_duty_cycle_right(1.0f);
+//  set_duty_cycle_left(1.0f);
+//  set_duty_cycle_right(1.0f);
 }
