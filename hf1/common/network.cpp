664b46c449048329e23c9718c4117619826687df
uavster <ignacio.mellado@gmail.com>
2023-05-05 14:02:45 -0700

    Network to local and vice versa conversion for 64 bit types.

--- hf1/common/network.cpp
+++ hf1/common/network.cpp
@@ -8,7 +8,7 @@ uint16_t FlipBytes(uint16_t v) {
   return *reinterpret_cast<uint16_t *>(p);
 }
 
-uint16_t FlipBytes(uint32_t v) {
+uint32_t FlipBytes(uint32_t v) {
   uint8_t *p = reinterpret_cast<uint8_t *>(&v);
   uint8_t tmp = p[0];
   p[0] = p[3];
@@ -19,10 +19,31 @@ uint16_t FlipBytes(uint32_t v) {
   return *reinterpret_cast<uint32_t *>(p);
 }
 
+uint64_t FlipBytes(uint64_t v) {
+  uint8_t *p = reinterpret_cast<uint8_t *>(&v);
+  uint8_t tmp = p[0];
+  p[0] = p[7];
+  p[7] = tmp;
+  tmp = p[1];
+  p[1] = p[6];
+  p[6] = tmp;
+  tmp = p[2];
+  p[2] = p[5];
+  p[5] = tmp;
+  tmp = p[3];
+  p[3] = p[4];
+  p[4] = tmp;
+  return *reinterpret_cast<uint64_t *>(p);
+}
+
 int16_t FlipBytes(int16_t v) {
   return FlipBytes(static_cast<uint16_t>(v));
 }
 
 int32_t FlipBytes(int32_t v) {
   return FlipBytes(static_cast<uint32_t>(v));
+}
+
+int64_t FlipBytes(int64_t v) {
+  return FlipBytes(static_cast<uint64_t>(v));
 }
--- hf1/common/network.h
+++ hf1/common/network.h
@@ -6,22 +6,28 @@
 enum Endianness { kLittleEndian, kBigEndian };
 
 uint16_t FlipBytes(uint16_t v);
-uint16_t FlipBytes(uint32_t v);
+uint32_t FlipBytes(uint32_t v);
+uint64_t FlipBytes(uint64_t v);
 int16_t FlipBytes(int16_t v);
 int32_t FlipBytes(int32_t v);
+int64_t FlipBytes(int64_t v);
 
 namespace {
   template<Endianness LocalEndianness, typename DataType> inline DataType n2l(DataType v) { return v; }
   template<> inline uint16_t n2l<Endianness::kBigEndian, uint16_t>(uint16_t v) { return FlipBytes(v); }
   template<> inline uint32_t n2l<Endianness::kBigEndian, uint32_t>(uint32_t v) { return FlipBytes(v); }
+  template<> inline uint64_t n2l<Endianness::kBigEndian, uint64_t>(uint64_t v) { return FlipBytes(v); }
   template<> inline int16_t n2l<Endianness::kBigEndian, int16_t>(int16_t v) { return FlipBytes(v); }
   template<> inline int32_t n2l<Endianness::kBigEndian, int32_t>(int32_t v) { return FlipBytes(v); }
+  template<> inline int64_t n2l<Endianness::kBigEndian, int64_t>(int64_t v) { return FlipBytes(v); }
 
   template<Endianness LocalEndianness, typename DataType> inline DataType l2n(DataType v) { return v; }
   template<> inline uint16_t l2n<Endianness::kBigEndian, uint16_t>(uint16_t v) { return FlipBytes(v); }
   template<> inline uint32_t l2n<Endianness::kBigEndian, uint32_t>(uint32_t v) { return FlipBytes(v); }
+  template<> inline uint64_t l2n<Endianness::kBigEndian, uint64_t>(uint64_t v) { return FlipBytes(v); }
   template<> inline int16_t l2n<Endianness::kBigEndian, int16_t>(int16_t v) { return FlipBytes(v); }
   template<> inline int32_t l2n<Endianness::kBigEndian, int32_t>(int32_t v) { return FlipBytes(v); }
+  template<> inline int64_t l2n<Endianness::kBigEndian, int64_t>(int64_t v) { return FlipBytes(v); }
 }
 
 template<Endianness LocalEndianness, typename DataType> DataType NetworkToLocal(DataType v) { return n2l<LocalEndianness, DataType>(v); }
