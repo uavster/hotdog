d828994615200184a6fa704fb1fa49c592e02e1e
uavster <ignacio.mellado@gmail.com>
2024-08-14 16:45:14 +0200

    Encoders now use the same FTM as the timer routines for a common time base.

--- hf1/arduino/encoders.cpp
+++ hf1/arduino/encoders.cpp
@@ -7,17 +7,17 @@ EncoderISR left_encoder_isr;
 EncoderISR right_encoder_isr;
 
 static void EncodersIsr() {
-  if (FTM0_C0SC & FTM_CSC_CHF) {
+  if (FTM2_C0SC & FTM_CSC_CHF) {
     if (left_encoder_isr != NULL) {
-      left_encoder_isr((GetTimerTicks() & 0xffff0000) | (FTM0_C0V & 0xffff));
+      left_encoder_isr((GetTimerTicks() & 0xffff0000) | (FTM2_C0V & 0xffff));
     }
-    FTM0_C0SC &= ~FTM_CSC_CHF;
+    FTM2_C0SC &= ~FTM_CSC_CHF;
   }
-  if (FTM0_C1SC & FTM_CSC_CHF) {
+  if (FTM2_C1SC & FTM_CSC_CHF) {
     if (right_encoder_isr != NULL) {
-      right_encoder_isr((GetTimerTicks() & 0xffff0000) | (FTM0_C1V & 0xffff));
+      right_encoder_isr((GetTimerTicks() & 0xffff0000) | (FTM2_C1V & 0xffff));
     }
-    FTM0_C1SC &= ~FTM_CSC_CHF;
+    FTM2_C1SC &= ~FTM_CSC_CHF;
   }
 }
 
@@ -28,10 +28,10 @@ void InitEncoders() {
     // Configure timer 0's channels 0 and 1 for input capture on rising edge
     // Table 36-67
     // CH0IE = 1 (interrupt enabled), MS0B:MS0A = 0 and ELS0B:ELS0A = 1 (capture on rising edge)
-    FTM0_C0SC = FTM_CSC_CHIE | FTM_CSC_ELSA;
-    PORTC_PCR1 = PORT_PCR_MUX(4);
-    FTM0_C1SC = FTM_CSC_CHIE | FTM_CSC_ELSA;
-    PORTC_PCR2 = PORT_PCR_MUX(4);
+    FTM2_C0SC = FTM_CSC_CHIE | FTM_CSC_ELSA;
+    PORTB_PCR18 = PORT_PCR_MUX(3);
+    FTM2_C1SC = FTM_CSC_CHIE | FTM_CSC_ELSA;
+    PORTB_PCR19 = PORT_PCR_MUX(3);
   }
 }
 
