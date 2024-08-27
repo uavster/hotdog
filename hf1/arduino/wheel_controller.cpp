a89fbb26de6a6a08e3ca036c3871d5e4b34137c4
uavster <ignacio.mellado@gmail.com>
2024-08-27 19:21:00 +0200

    Better wheel speed estimation and control.

--- hf1/arduino/wheel_controller.cpp
+++ hf1/arduino/wheel_controller.cpp
@@ -2,6 +2,7 @@
 #include "wheel_controller.h"
 #include "robot_model.h"
 #include "utils.h"
+#include <algorithm>
 
 #define kControlLoopPeriodSeconds 1e-2
 
@@ -10,24 +11,13 @@
 #define kSpeedModelFactor 0.041
 #define kSpeedModelSpeedOffset 0.66
 
-// Best params for 0.6 m/s
-// #define kP 20.0
-// #define kI 50.0
-// #define kD 0.0
-
-// // Best params for 0.15 m/s
-// #define kP 20.0
-// #define kI 10.0
-// #define kD 0.5
-
-// Best params for 0.4 m/s
+// Best params at 0.4 m/s
 #define kP 10.0
-#define kI 10.0
-#define kD 0.5
+#define kI 5.0
+#define kD 0 //0.5
 
-// Any wheel speed estimate above this value is rejected.
-// Meant to prevent too high estimates due to co-occuring encoder edges.
-#define kWheelRadiansPerSecondMax (2.0 / kWheelRadius) // [rad/s]
+#define kPWMDutyCycleMin 0.0f
+#define kPWMDutyCycleMax 1.0f
 
 int32_t left_wheel_num_ticks;
 int32_t right_wheel_num_ticks;
@@ -67,6 +57,8 @@ WheelSpeedController::WheelSpeedController(
     wheel_tick_count_getter_(*ASSERT_NOT_NULL(wheel_tick_count_getter)), 
     duty_cycle_setter_(*ASSERT_NOT_NULL(duty_cycle_setter)), 
     last_run_seconds_(-1),
+    average_wheel_speed_(0),
+    is_turning_forward_(true),
     pid_(kP, kI, kD)
 {
 }
@@ -79,6 +71,10 @@ float WheelSpeedController::DutyCycleFromLinearSpeed(float meters_per_second) co
   // 
   // This function computes the inverse of said model for both forward and backward speeds
   // and imposes duty cycle limits.
+  if (meters_per_second == 0) {
+    // If speed is 0, force the duty cycle to 0 to save power. 
+    return 0;
+  }
   if (meters_per_second >= kSpeedModelSpeedOffset) {
     return 1.0f;
   }
@@ -87,7 +83,7 @@ float WheelSpeedController::DutyCycleFromLinearSpeed(float meters_per_second) co
   }
   const float offset = kSpeedModelTimeConstant * log(kSpeedModelFactor) - kSpeedModelDutyCycleOffset;
   float duty_cycle = offset - kSpeedModelTimeConstant * log(kSpeedModelSpeedOffset - fabsf(meters_per_second));
-  duty_cycle = min(1.0f, max(0.0f, duty_cycle));
+  duty_cycle = std::clamp(duty_cycle, kPWMDutyCycleMin, kPWMDutyCycleMax);
   if (meters_per_second < 0) {
     duty_cycle = -duty_cycle;
   }
@@ -98,6 +94,7 @@ void WheelSpeedController::SetLinearSpeed(float meters_per_second) {
   time_start_ = GetTimerSeconds();
   num_wheel_ticks_start_ = wheel_tick_count_getter_();
   pid_.target(meters_per_second);
+  // pid_.ResetIntegrator();
 }
 
 void WheelSpeedController::SetAngularSpeed(float radians_per_second) {
@@ -105,12 +102,62 @@ void WheelSpeedController::SetAngularSpeed(float radians_per_second) {
 }
 
 void WheelSpeedController::RunAfterPeriod(TimerNanosType now_nanos, TimerNanosType nanos_since_last_call) {
-  float seconds_since_start = SecondsFromNanos(now_nanos) - time_start_;
-  float average_wheel_speed = kWheelRadius * kRadiansPerWheelTick * (wheel_tick_count_getter_() - num_wheel_ticks_start_) / seconds_since_start;
-  const float pid_output = pid_.update(average_wheel_speed, SecondsFromNanos(nanos_since_last_call));
+  // Estimate wheel turn direction.
+  // Assume the wheel is turning in the commanded direction because we cannot sense it.
+  // If the target is zero, it does not work as proxy of the speed sign, so infer the 
+  // sign from the previous pid output. Without this, when the target is zero, a speed
+  // error can destabilize the control loop and make the robot drive backwards 
+  // indefinitely.
+  bool is_turning_forward = pid_.target() > 0 || (pid_.target() == 0 && pid_.output() >= 0);
+  if (is_turning_forward != is_turning_forward_) {
+    // The turn direction changed: reset the speed estimation.
+    average_wheel_speed_ = 0;
+  }
+  is_turning_forward_ = is_turning_forward;
+
+  // Estimate wheel speed.
+  const float seconds_since_start = SecondsFromNanos(now_nanos) - time_start_;
+  int num_encoder_ticks = wheel_tick_count_getter_() - num_wheel_ticks_start_;
+  if (num_encoder_ticks > 0) {
+    // Only update the speed estimate if get got any encoder ticks since the last change of
+    // target speed. Otherwise, we'd get 0 after every change and the consequent control 
+    // peak.
+    float cur_avg_speed = kWheelRadius * kRadiansPerWheelTick * num_encoder_ticks / seconds_since_start;
+    if (!is_turning_forward_) {
+      // Assume the wheel is turning in the commanded direction because we cannot sense it.
+      cur_avg_speed = -cur_avg_speed;
+    }
+    average_wheel_speed_ = cur_avg_speed;
+  }
+
+  // Update duty cycle with the speed estimate.
+  const float pid_output = pid_.update(average_wheel_speed_, SecondsFromNanos(nanos_since_last_call));
   float speed_command = pid_.target() + pid_output;
-  speed_command = max(speed_command, 0.0f);
+  if ((is_turning_forward && speed_command < 0) || (!is_turning_forward && speed_command > 0)) {
+    // Avoid speed commands opposite to the driving direction as that can make the wheel 
+    // slip and hurt localization, making the error irrecoverable by the trajectory
+    // controller.
+    speed_command = 0;
+  }
   const float duty_cycle = DutyCycleFromLinearSpeed(speed_command);
   duty_cycle_setter_(duty_cycle);
-  // Serial.printf("t:%f v:%f pid:%f c:%f d:%f\n", pid_.target(), average_wheel_speed, pid_output, speed_command, duty_cycle);
+
+  // Serial.printf("t:%f v:%f pid:%f c:%f d:%f\n", pid_.target(), average_wheel_speed_, pid_output, speed_command, duty_cycle);
+}
+
+float WheelSpeedController::GetMaxLinearSpeed() const {
+  const float offset = kSpeedModelTimeConstant * log(kSpeedModelFactor) - kSpeedModelDutyCycleOffset;
+  return kSpeedModelSpeedOffset - exp((offset - kPWMDutyCycleMax) / kSpeedModelTimeConstant);
+}
+
+float WheelSpeedController::GetMaxAngularSpeed() const {
+  return GetMaxLinearSpeed() / kWheelRadius;
+}
+
+float WheelSpeedController::GetMinLinearSpeed() const {
+  return -GetMaxLinearSpeed();
+}
+
+float WheelSpeedController::GetMinAngularSpeed() const {
+  return -GetMaxAngularSpeed();
 }
--- hf1/arduino/wheel_controller.h
+++ hf1/arduino/wheel_controller.h
@@ -18,9 +18,23 @@ public:
   // No ownership of the pointee is taken by this object.
   WheelSpeedController(WheelTickCountGetter * const wheel_tick_count_getter, DutyCycleSetter * const duty_cycle_setter);
 
+  // Returns the maximum attainable linear speed [m/s].
+  float GetMaxLinearSpeed() const;
+  // Returns the maximum attainable angular speed [rad/s].
+  float GetMaxAngularSpeed() const;
+  // Returns the minimum (most negative) attainable linear speed [m/s].
+  float GetMinLinearSpeed() const;
+  // Returns the minimum (most negative) attainable angular speed [rad/s].
+  float GetMinAngularSpeed() const;
+
+  // Sets the target speed of the wheel in m/s (linear) or rad/s (angular). Negative values 
+  // make the wheel move backwards. The caller must ensure the current speed is zero before 
+  // setting a target speed opposite to the current direction of movement. Otherwise, the 
+  // sudden change would introduce localization error, and the robot might even tumble.
   void SetLinearSpeed(float meters_per_second);
   void SetAngularSpeed(float radians_per_second);
 
+  // Periodically updates the speed controller.
   void RunAfterPeriod(TimerNanosType now_nanos, TimerNanosType nanos_since_last_call) override;
 
 private:
@@ -31,7 +45,9 @@ private:
 
   float last_run_seconds_;
   float time_start_;
-  int32_t num_wheel_ticks_start_;  
+  int32_t num_wheel_ticks_start_;
+  float average_wheel_speed_;
+  bool is_turning_forward_;
   
   PID pid_;
 };
