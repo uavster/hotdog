caef274f2a2951e4e2004f552cf3ce609df7155b
uavster <ignacio.mellado@gmail.com>
2024-08-27 19:19:57 +0200

    In anti-windup, integrator set to error instead of 0; PID has output() function.

--- hf1/arduino/pid.h
+++ hf1/arduino/pid.h
@@ -5,11 +5,21 @@ class PID {
 public:
   PID(float p, float i, float d);
 
+  // Sets the target value.
   void target(float target) { target_ = target; }
+  // Returns the target value.
   float target() const { return target_; }
+
+  // Sets the integrator term to zero. When the target is updated to a different value, 
+  // the integrator will keep pushing the output towards the old target, so it is better
+  // to reset it.
   void ResetIntegrator() { integrator_ = 0; }
 
+  // Updates the PID output and returns it.
   float update(float feedback, float seconds_since_last_update);
+
+  // Returns the PID output.
+  float output() const { return output_; }
 
 private:
   float kp_;
@@ -18,6 +28,7 @@ private:
   float target_;
   float integrator_;
   float error_;
+  float output_;
 };
 
 #endif  // PID_INCLUDED_
