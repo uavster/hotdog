717e1cd4cedf226a76e8fa128217c89c5f3759cc
uavster <ignacio.mellado@gmail.com>
2024-08-27 19:22:01 +0200

    Base speed controller supports all four cases of linear and angular directions.

--- hf1/arduino/base_controller.cpp
+++ hf1/arduino/base_controller.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "base_controller.h"
 #include "utils.h"
 #include "robot_model.h"
@@ -10,14 +11,52 @@
 #define kKyaw 16.0    // [1/m]
 
 BaseSpeedController::BaseSpeedController(WheelSpeedController *left_wheel, WheelSpeedController *right_wheel) 
-  : left_wheel_(*ASSERT_NOT_NULL(left_wheel)), right_wheel_(*ASSERT_NOT_NULL(right_wheel)) {}
+  : left_wheel_(*ASSERT_NOT_NULL(left_wheel)), right_wheel_(*ASSERT_NOT_NULL(right_wheel)) {
+  ASSERT(left_wheel_.GetMaxAngularSpeed() == right_wheel_.GetMaxAngularSpeed());
+}
 
 void BaseSpeedController::SetTargetSpeeds(const float linear, const float angular) {
-  const float linear_component = linear / kWheelRadius;
-  const float angular_component = (angular * kRobotDistanceBetweenTireCenters) / (2 * kWheelRadius);
-  const float wheel_speed_l = min(0.6 / kWheelRadius, max(0, linear_component - angular_component));
-  const float wheel_speed_r = min(0.6 / kWheelRadius, max(0, linear_component + angular_component));
+  target_speed_linear_ = linear;
+  target_speed_angular_ = angular;
+  if (linear >= 0) {
+    target_speed_linear_ = min(target_speed_linear_, left_wheel_.GetMaxLinearSpeed());
+    if (angular >= 0) {
+      const float angular_max = 2 * (left_wheel_.GetMaxLinearSpeed() - target_speed_linear_) / kRobotDistanceBetweenTireCenters;
+      if (angular > angular_max) {
+        target_speed_angular_ = angular_max;
+      }
+    } else {
+      const float angular_min = -2 * (left_wheel_.GetMaxLinearSpeed() - target_speed_linear_) / kRobotDistanceBetweenTireCenters;
+      if (angular < angular_min) {
+        target_speed_angular_ = angular_min;
+      }
+    }
+  } else {
+    target_speed_linear_ = max(target_speed_linear_, left_wheel_.GetMinLinearSpeed());
+    if (angular >= 0) {
+      const float angular_max = -2 * (left_wheel_.GetMinLinearSpeed() - target_speed_linear_) / kRobotDistanceBetweenTireCenters;
+      if (angular > angular_max) {
+        target_speed_angular_ = angular_max;
+      }
+    } else {
+      const float angular_min = 2 * (left_wheel_.GetMinLinearSpeed() - target_speed_linear_) / kRobotDistanceBetweenTireCenters;
+      if (angular < angular_min) {
+        target_speed_angular_ = angular_min;
+      }
+    }
+  }
+  if (angular != target_speed_angular_) {
+    // Prevents NaN linear speed when angular speed is zero.
+    target_speed_linear_ = (target_speed_linear_ * target_speed_angular_) / angular;
+  }
+  
+  const float linear_component = target_speed_linear_ / kWheelRadius;
+  const float angular_component = (target_speed_angular_ * kRobotDistanceBetweenTireCenters) / (2 * kWheelRadius);
+  const float wheel_speed_l = linear_component - angular_component;
+  const float wheel_speed_r = linear_component + angular_component;
+
   Serial.printf("l:%f r:%f\n", wheel_speed_l, wheel_speed_r);
+
   left_wheel_.SetAngularSpeed(wheel_speed_l);
   right_wheel_.SetAngularSpeed(wheel_speed_r);
 }
@@ -57,11 +96,10 @@ void BaseStateController::RunAfterPeriod(TimerNanosType now_nanos, TimerNanosTyp
   base_speed_controller_.SetTargetSpeeds(min(0.4, max(0, linear_speed_command)), min(0.8, max(-0.8, angular_speed_command)));
 }
 
-void BaseStateController::SetTargetState(const Point &center_position_target, float yaw_target, float reference_forward_speed, float max_forward_speed, float reference_angular_speed) {
+void BaseStateController::SetTargetState(const Point &center_position_target, float yaw_target, float reference_forward_speed, float reference_angular_speed) {
   center_position_target_ = center_position_target;
   yaw_target_ = yaw_target;
   reference_forward_speed_ = reference_forward_speed;
-  max_forward_speed_ = max_forward_speed;
   reference_angular_speed_ = reference_angular_speed;
 }
 
--- hf1/arduino/base_controller.h
+++ hf1/arduino/base_controller.h
@@ -12,19 +12,34 @@ public:
   // Does not take ownership of any pointee.
   BaseSpeedController(WheelSpeedController *left_wheel, WheelSpeedController *right_wheel);
 
+  // Sets the target linear and angular speeds. The speeds might not be attainable by the 
+  // wheels. If the linear speed is not attainable, the target is clamped to the wheel limit. 
+  // Then, if the angular speed is not attainable, it is clamped to the angular speed limit 
+  // given the updated target linear speed, and the linear speed is adjusted to keep the 
+  // radius given by the updated linear speed and the original angular speed.
   void SetTargetSpeeds(float linear, float angular);
+
+  // Returns the target linear speed adjusted to what the wheels can do.
+  float target_linear_speed() const { return target_speed_linear_; }
+  // Returns the target angular speed adjusted to what the wheels can do.
+  float target_angular_speed() const { return target_speed_angular_; }
+  // Returns the radius of the curve given the adjusted target speeds.
+  float curve_radius() const { return target_linear_speed() / target_angular_speed(); }
+
   void Run();
 
 private:
   WheelSpeedController &left_wheel_;
   WheelSpeedController &right_wheel_;
+  float target_speed_linear_;
+  float target_speed_angular_;
 };
 
 class BaseStateController : public PeriodicRunnable {
 public:
   BaseStateController(BaseSpeedController *base_speed_controller);
 
-  void SetTargetState(const Point &center_position_target, float yaw_target, float reference_forward_speed, float max_forward_speed, float reference_angular_speed = 0.0f);
+  void SetTargetState(const Point &center_position_target, float yaw_target, float reference_forward_speed, float reference_angular_speed = 0.0f);
 
   void RunAfterPeriod(TimerNanosType now_nanos, TimerNanosType nanos_since_last_call) override;
   void Run();
@@ -35,7 +50,6 @@ private:
   float yaw_target_;
   float reference_forward_speed_;
   float reference_angular_speed_;
-  float max_forward_speed_;
 };
 
 #endif  // ROBOT_SPEED_CONTROLLER_
