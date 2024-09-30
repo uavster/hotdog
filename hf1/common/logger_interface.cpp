ae60eada1721574fe0e993f114bbd99ef828c6d8
uavster <ignacio.mellado@gmail.com>
2024-10-01 00:21:28 +0200

    Logger interface has info, warning and error levels.

--- hf1/common/logger_interface.cpp
+++ hf1/common/logger_interface.cpp
@@ -1,3 +1,6 @@
+#ifndef LOGGER_INTERFACE_INCLUDED__
+#define LOGGER_INTERFACE_INCLUDED__
+
 #include "logger_interface.h"
 #ifdef ARDUINO
 #include <Arduino.h>
@@ -13,6 +16,10 @@ LoggerInterface *SetLogger(LoggerInterface *new_logger) {
   LoggerInterface *old_logger = logger_;
   logger_ = new_logger;
   return old_logger;
+}
+
+LoggerInterface *GetLogger() {
+  return logger_;
 }
 
 void DefaultLogger::Fatal(const char *expr, const char *file_name, int line, const char *msg) {
@@ -31,4 +38,27 @@ void DefaultLogger::Fatal(const char *expr, const char *file_name, int line, con
 #endif    
 }
 
+void DefaultLogger::Message(const char *level, const char *file_name, int line, const char *msg) {
+#ifdef ARDUINO
+  Serial.printf("[%s] %s:%d: %s\n", level, file_name, line, msg);
+#else
+  printf("[%s] %s:%d: %s\n", level, file_name, line, msg);
+#endif    
+}
 
+void DefaultLogger::Info(const char *file_name, int line, const char *msg) {
+  const static char level[] = "INFO";
+  Message(level, file_name, line, msg);
+}
+
+void DefaultLogger::Warning(const char *file_name, int line, const char *msg) {
+  const static char level[] = "WARNING";
+  Message(level, file_name, line, msg);
+}
+
+void DefaultLogger::Error(const char *file_name, int line, const char *msg) {
+  const static char level[] = "ERROR";
+  Message(level, file_name, line, msg);
+}
+
+#endif  // LOGGER_INTERFACE_INCLUDED__
--- hf1/common/logger_interface.h
+++ hf1/common/logger_interface.h
@@ -3,19 +3,31 @@
 
 class LoggerInterface {
 public:
+  virtual void Info(const char *file_name, int line, const char *msg) = 0;
+  virtual void Warning(const char *file_name, int line, const char *msg) = 0;
+  virtual void Error(const char *file_name, int line, const char *msg) = 0;
   virtual void Fatal(const char *expr, const char *file_name, int line, const char *msg) = 0;
 };
 
 class DefaultLogger : public LoggerInterface {
 public:
+  virtual void Info(const char *file_name, int line, const char *msg);
+  virtual void Warning(const char *file_name, int line, const char *msg);
+  virtual void Error(const char *file_name, int line, const char *msg);
   virtual void Fatal(const char *expr, const char *file_name, int line, const char *msg);
+
+protected:
+  void Message(const char *level, const char *file_name, int line, const char *msg);
 };
 
-extern LoggerInterface *logger_;
-
 LoggerInterface *SetLogger(LoggerInterface *new_logger);
+LoggerInterface *GetLogger();
 
-#define ASSERT(x) if (!(x)) { logger_->Fatal(#x, __FILE__, __LINE__, nullptr); }
-#define ASSERTM(x, m) if (!(x)) { logger_->Fatal(#x, __FILE__, __LINE__, m); }
+#define LOG_INFO(m) (GetLogger()->Info(__FILE__, __LINE__, m))
+#define LOG_WARNING(m) (GetLogger()->Warning(__FILE__, __LINE__, m))
+#define LOG_ERROR(m) (GetLogger()->Error(__FILE__, __LINE__, m))
+
+#define ASSERT(x) if (!(x)) { GetLogger()->Fatal(#x, __FILE__, __LINE__, nullptr); }
+#define ASSERTM(x, m) if (!(x)) { GetLogger()->Fatal(#x, __FILE__, __LINE__, m); }
 
 #endif  // LOGGER_INTERFACE_
