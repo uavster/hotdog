833d6fe9e92932e02a940d24dcf747c2a57eac82
uavster <ignacio.mellado@gmail.com>
2024-10-10 11:52:09 +0200

    Cleanup commented code.

--- hf1/arduino/arduino.ino
+++ hf1/arduino/arduino.ino
@@ -26,15 +26,11 @@ Logger logger;
 P2PByteStreamArduino byte_stream(&Serial1);
 TimerArduino timer;
 GUIDFactory guid_factory;
-// P2PPacketInputStream<8, kLittleEndian> p2p_input_stream(&byte_stream, &timer);
-// P2PPacketOutputStream<1, kLittleEndian> p2p_output_stream(&byte_stream, &timer);
 P2PPacketStreamArduino p2p_stream(&byte_stream, &timer, guid_factory);
 TimeSyncServer<kP2PInputCapacity, kP2POutputCapacity, kP2PLocalEndianness> time_sync_server(&p2p_stream, &timer);
 HeadPoseServer head_pose_server(&p2p_stream);
 
 TimerNanosType last_msg_time_ns = 0;
-
-// uint8_t rx_buffer[256];
 
 int last_received_packet_value[P2PPriority::kNumLevels];
 int received_packets[P2PPriority::kNumLevels];
@@ -88,7 +84,6 @@ void setup() {
   InitRobotStateEstimator();
 
   Serial.println("Initializing inter-board communications...");
-  // Serial1.addMemoryForRead(rx_buffer, 256);
   Serial1.begin(1000000, SERIAL_8N1);
 
   Serial.println("Initializing motors...");
@@ -110,50 +105,6 @@ void setup() {
   time_sync_server.Init();
 
   last_msg_time_ns = GetTimerNanoseconds();
-
-  // UART0_S2 |= UART_S2_LBKDE;
-  // Clear FE.
-  // noInterrupts();
-  // uint8_t uart_s1 = UART0_S1;  
-  // ++uart_s1;
-  // uint8_t uart_d = UART0_D;
-  // ++uart_d;
-  // interrupts();
-  // Serial.println("After clear");
-  // Serial.printf("c1=%x c2=%x c3=%x c4=%x c5=%x\n", UART0_C1, UART0_C2, UART0_C3, UART0_C4, UART0_C5);
-  // Serial.printf("s1=%x s2=%x\n", UART0_S1, UART0_S2);
-
-  //  timer0_set_write_protected();
-  /*
-  PORTA_PCR12 = PORT_PCR_MUX(1);
-  SIM_SOPT4 &= SIM_SOPT4_FTM1CH0SRC(0);
-  SIM_SCGC5 |= 1 << 10;
-  SIM_SCGC6 |= 1 << 25;
-*/
-  //  timer1_clear_write_protected();
-  // FTMEN = 1 (enable all registers)
-  //  FTM1_MODE |= FTM_MODE_FTMEN;
-
-  // Pin 1 controls direction of the right wheel.
-  //  pinMode(17, OUTPUT);
-  //  digitalWrite(17, 1);
-
-
-  //  timer1_set_write_protected();
-  //  analogWriteFrequency(16, 100);
-  //  analogWrite(16, 64);
-
-  /*
-  // PORTD_PCR1 = PORT_PCR_MUX(2);
-  // PORTD_PCR2 = PORT_PCR_MUX(2);
-  // PORTD_PCR3 = PORT_PCR_MUX(2);
-  SPI.setSCK(14);
-  SPI.setMISO(8);
-  SPI.setMOSI(7);
-  SPI.begin();  
-  pinMode(2, OUTPUT);   // SPI CS
-  digitalWrite(2, HIGH);
-  */
   
   Serial.println("Ready.");
 
@@ -373,197 +324,6 @@ void loop() {
   // }
 
   // return;
-
-/*  
-  if (next_global_time_event_s == -1ULL) {
-    next_global_time_event_s = (timer.GetGlobalNanoseconds() + 1000000000ULL) / 1000000000ULL;
-  }
-  uint64_t cur_global_time_ns = timer.GetGlobalNanoseconds();
-	if (cur_global_time_ns / 1000000000ULL >= next_global_time_event_s) {
-    char tmp[32];
-    Uint64ToString(cur_global_time_ns, tmp);
-    Serial.println(tmp);
-    next_global_time_event_s = (cur_global_time_ns + 1000000000ULL) / 1000000000ULL;
-  }
-
-  p2p_stream.input().Run();
-  p2p_stream.output().Run();
-  time_sync_server.Run();
-
-  return;
-*/
-
-/*  TimerNanosType now_ns = GetTimerNanoseconds();
-
-  // Working values:
-  // 9600 bps -> 230
-  // 115200 bps -> 18
-  // 1000000 bps -> 2
-
-  int len = 0xa8;
-  if (now_ns - last_sent_packet_nanos >= 9000000)
-  {
-    last_sent_packet_nanos = now_ns;
-    P2PPriority priority = P2PPriority::Level::kHigh;
-    current_packet_view = p2p_stream.output().NewPacket(priority);
-    if (current_packet_view.ok()) {
-      for (int i = 0; i < len; ++i) {
-        reinterpret_cast<uint8_t *>(current_packet_view->content())[i] = 0;
-      }
-      *reinterpret_cast<uint8_t *>(current_packet_view->content()) = sent_packets[priority];
-      current_packet_view->length() = len; //sizeof(uint8_t);
-      ASSERT(p2p_stream.output().Commit(priority, true));       
-
-      ++sent_packets[priority];
-      // ++len;
-      // if (len == 0xa9) { len = 1; }
-    }
-  }
-
-  len = 0xa8;
-  P2PPriority priority = P2PPriority::Level::kLow;
-  current_packet_view = p2p_stream.output().NewPacket(priority);
-  if (current_packet_view.ok()) {
-    for (int i = 0; i < len; ++i) {
-      reinterpret_cast<uint8_t *>(current_packet_view->content())[i] = 0;
-    }
-    *reinterpret_cast<uint8_t *>(current_packet_view->content()) = sent_packets[priority];
-    current_packet_view->length() = len; //sizeof(uint8_t);
-    ASSERT(p2p_stream.output().Commit(priority, false)); 
-    ++sent_packets[priority];
-    // ++len;
-    // if (len == 0xa9) { len = 1; }
-  }
-
-  if (now_ns - last_msg_time_ns >= 1e9) {
-    Serial.print("tx:");
-    for (int i = 0; i < P2PPriority::kNumLevels; ++i) {
-      Serial.printf("%d ", sent_packets[i] - last_sent_packets[i]);
-    }
-    Serial.print(", rx:");
-    for (int i = 0; i < P2PPriority::kNumLevels; ++i) {
-      Serial.printf("%d ", received_packets[i] - last_received_packets[i]);
-    }
-    Serial.print(", lost:");
-    for (int i = 0; i < P2PPriority::kNumLevels; ++i) {
-      Serial.printf("%d ", lost_packets[i]);
-    }
-    Serial.print(", tx_delay(ns):");
-    for (int i = 0; i < P2PPriority::kNumLevels; ++i) {
-      uint64_t delay = p2p_stream.output().stats().average_packet_delay_per_byte_ns(i);
-      if (delay != -1ULL) {
-        char str[24];
-        Uint64ToString(delay, str);
-        Serial.printf("%s ", str);
-      } else {
-        Serial.printf("? ");
-      }
-    }
-    Serial.print(", rx_delay(ns):");
-    for (int i = 0; i < P2PPriority::kNumLevels; ++i) {
-      uint64_t delay = p2p_stream.input().stats().average_packet_delay_per_byte_ns(i);
-      if (delay != -1ULL) {
-        char str[24];
-        Uint64ToString(delay, str);
-        Serial.printf("%s ", str);
-      } else {
-        Serial.printf("? ");
-      }
-    }
-    Serial.println("");
-    for (int i = 0; i < P2PPriority::kNumLevels; ++i) {
-      last_sent_packets[i] = sent_packets[i];
-      last_received_packets[i] = received_packets[i];
-    }
-    last_msg_time_ns = now_ns;
-  }
-
-  if (p2p_stream.input().OldestPacket().ok()) {
-    // Serial.write(p2p_input_stream.OldestPacket()->content(), p2p_input_stream.OldestPacket()->length());
-    // Serial.print("p: ");
-    // for (int i = 0; i < p2p_input_stream.OldestPacket()->length(); ++i) {
-    //   Serial.printf("%x ", p2p_input_stream.OldestPacket()->content()[i]);
-    // }
-    // Serial.println();
-    
-    P2PPriority priority = p2p_stream.input().OldestPacket()->priority();
-    ++received_packets[priority];
-    if (last_received_packet_value[priority] >= 0) {
-      int diff = p2p_stream.input().OldestPacket()->content()[0] - last_received_packet_value[priority];
-      if (diff > 0) lost_packets[priority] += diff - 1;
-      else lost_packets[priority] += 256 + diff - 1;
-    }
-    last_received_packet_value[priority] = p2p_stream.input().OldestPacket()->content()[0];
-    p2p_stream.input().Consume(priority);
-  }
-
-  p2p_stream.input().Run();
-  p2p_stream.output().Run();
-
-  return;
-  */
-  /*
- if (SPI.pinIsChipSelect(2)) { Serial.println("2 is CS"); }
-  else { Serial.println("2 is NOT CS");}
-  if (SPI.pinIsSCK(14)) { Serial.println("14 is SCK"); }
-  else { Serial.println("14 is NOT SCK");}
-  if (SPI.pinIsMISO(8)) { Serial.println("8 is MISO"); }
-  else { Serial.println("8 is NOT MISO");}
-  if (SPI.pinIsMOSI(7)) { Serial.println("7 is MOSI"); }
-  else { Serial.println("7 is NOT MOSI");}
-
-    SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE1));
-    Serial.println("Enabling CS");
-    digitalWrite(2, LOW); // select device
-    delay(5000);
-    Serial.println("Transferring");
-    
-    char buffer[1000];
-    for (int i = 0; i < (int)sizeof(buffer); ++i) buffer[i] = 0xaa;
-    for (int i = 0; i < 100; ++i) {
-    SPI.transfer(buffer, sizeof(buffer));
-    }
-    Serial.println("Disabling CS");
-    digitalWrite(2, HIGH);
-    SPI.endTransaction();
-    delay(5000);
-    return;
-*/
-  /*
-    Serial1.write(a);
-    a++;
-    if (a > 'z') { a = '0'; }
-
-  int k = Serial1.read();
-  if (k >= 0) {
-    Serial.println(k);
-  }
-  return;
-  */
-
-  /*  if (event_buffer.NumEvents() > 0) {
-    const Event event = event_buffer.Read();
-    Uint64ToString(event.ticks, format_buffer);
-    switch(event.type) {
-      case kLeftWheelTick:
-        Serial.print("L: ");
-        break;
-      case kRightWheelTick:
-        Serial.print("R: ");
-        break;
-    }
-    Serial.println(format_buffer);
-  }
-*/
-
-  /*  static float t = 0; 
-  float left_command = -sin(2*3.14159f*0.1*t);
-  float right_command = sin(2*3.14159f*0.1*t); 
-  set_duty_cycle_left(left_command);
-  set_duty_cycle_right(right_command);
-  t += 0.00005f;
-*/
-
 
 // #ifdef kAutomaticTransitions
 //   if (nodding_start_time_ns == -1ULL) {
