295017ba3190880f559a8fdd087105928238de5a
uavster <ignacio.mellado@gmail.com>
2024-12-03 19:42:54 +0100

    Test at 1kHz pwm.

--- hf1/arduino/arduino.ino
+++ hf1/arduino/arduino.ino
@@ -96,22 +96,23 @@ void setup() {
   //   waypoints[i+3*points_per_segment] = BaseWaypoint((i+3*points_per_segment) * 0.3, BaseTargetState({ BaseStateVars(Point(0, -1+0.1*i), 0) }));
   // }
 
-  // const int num_waypoints = sizeof(waypoints) / sizeof(waypoints[0]);
-  // const float total_trajectory_seconds = 20.0;
-  // for (int i = 0; i < num_waypoints; ++i) {
-  //   const float t = i * total_trajectory_seconds / num_waypoints;
-  //   const float w = 2 * M_PI / total_trajectory_seconds;
-  //   const float x = sin(w * t);
-  //   const float y = -1 + cos(w * t);
-  //   waypoints[i] = BaseWaypoint(t, BaseTargetState({ BaseStateVars(Point(x, y), 0) }));
-  // }
+  const int num_waypoints = sizeof(waypoints) / sizeof(waypoints[0]);
+  const float total_trajectory_seconds = 20.0;
+  for (int i = 0; i < num_waypoints; ++i) {
+    const float t = i * total_trajectory_seconds / num_waypoints;
+    const float w = 2 * M_PI / total_trajectory_seconds;
+    const float x = sin(w * t);
+    const float y = -1 + cos(w * t);
+    waypoints[i] = BaseWaypoint(t, BaseTargetState({ BaseStateVars(Point(x, y), 0) }));
+  }
+  base_trajectory_controller.trajectory(BaseTrajectoryView(num_waypoints, waypoints).EnableLooping(/*after_seconds=*/total_trajectory_seconds / num_waypoints).EnableInterpolation({ .type = InterpolationType::kLinear, .num_sampling_points = 40 }));
 
-  const int num_waypoints = 4;
-  waypoints[0] = BaseWaypoint(0, BaseTargetState({ BaseStateVars(Point(0, 0), 0) }));
-  waypoints[1] = BaseWaypoint(3, BaseTargetState({ BaseStateVars(Point(1, 0), 0) }));
-  waypoints[2] = BaseWaypoint(6, BaseTargetState({ BaseStateVars(Point(1, -1), 0) }));
-  waypoints[3] = BaseWaypoint(9, BaseTargetState({ BaseStateVars(Point(0, -1), 0) }));
-  base_trajectory_controller.trajectory(BaseTrajectoryView(num_waypoints, waypoints).EnableLooping(/*after_seconds=*/3.0).EnableInterpolation({ .type = InterpolationType::kLinear, .num_sampling_points = 40 }));
+  // const int num_waypoints = 4;
+  // waypoints[0] = BaseWaypoint(0, BaseTargetState({ BaseStateVars(Point(0, 0), 0) }));
+  // waypoints[1] = BaseWaypoint(3, BaseTargetState({ BaseStateVars(Point(1, 0), 0) }));
+  // waypoints[2] = BaseWaypoint(6, BaseTargetState({ BaseStateVars(Point(1, -1), 0) }));
+  // waypoints[3] = BaseWaypoint(9, BaseTargetState({ BaseStateVars(Point(0, -1), 0) }));
+  // base_trajectory_controller.trajectory(BaseTrajectoryView(num_waypoints, waypoints).EnableLooping(/*after_seconds=*/3.0).EnableInterpolation({ .type = InterpolationType::kLinear, .num_sampling_points = 40 }));
 
   base_trajectory_controller.StartTrajectory();
 }
