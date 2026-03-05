de7feafddb1162e02edd14eb210aec40aed7b156
uavster <ignacio.mellado@gmail.com>
2026-03-05 13:55:10 +0100

    Clarify role of TimerInterface and its subclasses.

--- hf1/arduino/timer_arduino.h
+++ hf1/arduino/timer_arduino.h
@@ -4,6 +4,10 @@
 #include "timer_interface.h"
 #include "timer.h"
 
+// This class is meant to abstract access to the platform's timer for dependency injection.
+// Classes to be called from unit tests should take a TimerInterface object during initialization
+// to access time, and refrain from calling the system functions directly. In this way, test suites
+// can instantiate them with a mock timer.
 class TimerArduino : public TimerInterface {
 public:
   virtual uint64_t GetLocalNanoseconds() const {
