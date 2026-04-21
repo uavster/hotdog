1a87e606e2032b60ac01f571d1261e7e42dd2fb3
uavster <ignacio.mellado@gmail.com>
2026-04-21 11:12:16 +0200

    NewAbort() creates abort packet.

--- hf1/arduino/p2p_action_server.hh
+++ hf1/arduino/p2p_action_server.hh
@@ -40,3 +40,17 @@ StatusOr<P2PActionPacketAdapter<TProgress>> P2PActionHandler<TRequest, TReply, T
   header->request_id = request_id();
   return P2PActionPacketAdapter<TProgress>(this, *maybe_packet);
 }
+
+template<typename TRequest, typename TReply, typename TProgress>
+StatusOr<P2PActionPacketAdapter<TReply>> P2PActionHandler<TRequest, TReply, TProgress>::NewAbort() {
+  StatusOr<P2PMutablePacketView> maybe_packet = p2p_stream().output().NewPacket(request_priority());
+  if (!maybe_packet.ok()) {
+    return maybe_packet.status();
+  }
+  maybe_packet->length() = sizeof(P2PApplicationPacketHeader) + sizeof(TReply);
+  P2PApplicationPacketHeader *header = reinterpret_cast<P2PApplicationPacketHeader *>(maybe_packet->content());
+  header->action = action();
+  header->stage = P2PActionStage::kCancel;
+  header->request_id = request_id();
+  return P2PActionPacketAdapter<TReply>(this, *maybe_packet);
+}
