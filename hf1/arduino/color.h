d73f7949e0b555dd6da857c742690eac576645a0
uavster <ignacio.mellado@gmail.com>
2026-04-27 19:36:42 +0200

    HSV constants.

--- hf1/arduino/color.h
+++ hf1/arduino/color.h
@@ -31,7 +31,7 @@ private:
 // A color in HSV space (Hue, Saturation, Value).
 // Each component is between 0 to 1.
 // For saturation, 0 is least colorful and 1 is most colorful.
-// For value, 0 is least dark and 1 is most dark.
+// For value, 0 is darkest and 1 is brighter.
 // For hue, it's the fraction of a turn around the color circle, where 
 // red is at 0 degrees (h=0), yellow is at 60 degrees (h=1/6), green is at 120 degrees (h=1/3), 
 // cyan is at 180 degrees (h=1/2), blue is at 240 degrees (h=2/3) and purple is at 300 degrees (h=5/6).
@@ -56,5 +56,18 @@ private:
   float s_;
   float v_;
 };
+
+constexpr float kRedHue = 0.0f;
+constexpr float kYellowHue = 1 / 6.0f;
+constexpr float kGreenHue = 2 / 6.0f;
+constexpr float kCyanHue = 3 / 6.0f;
+constexpr float kBlueHue = 4 / 6.0f;
+constexpr float kPurpleHue = 5 / 6.0f;
+
+constexpr float kLowestSaturation = 0.0f;
+constexpr float kHighestSaturation = 0.0f;
+
+constexpr float kDarkestValue = 0.0f;
+constexpr float kBrightestValue = 1.0f;
 
 #endif  // COLOR_INCLUDED_
